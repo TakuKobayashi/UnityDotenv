86d13ff16d15fdc1c3a671e79376775ef2bc8b67
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2021-03-03 16:36:08 +0900

    copy to the Unity Project

---
+++ Unity/Assets/DotenvSample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+
+namespace UnityDotenv
+{
+    public class DotenvSample : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log(Application.streamingAssetsPath + "/.env");
+            IEnumerable<KeyValuePair<string, string>> envValuePaur = Env.Load(Application.streamingAssetsPath + "/.env");
+            foreach (KeyValuePair<string, string> kvp in envValuePaur)
+            {
+                Debug.Log(kvp.Key + ":" + kvp.Value);
+            }
+
+            var environmentVariables = Environment.GetEnvironmentVariables();
+            Debug.Log(environmentVariables);
+            foreach (DictionaryEntry de in environmentVariables)
+            {
+                Debug.Log(de.Key + ":" + de.Value);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Unity/Assets/UnityDotenv/Scripts/EnvPreloader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class EnvPreloader
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void OnBeforeSceneLoadRuntimeMethod()
+    {
+        Debug.Log("Before first Scene loaded");
+    }
+}
