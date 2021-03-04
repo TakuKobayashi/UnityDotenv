1c6e58037b48c530e84deac51b90b91e6395720c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2021-03-04 17:23:12 +0900

    struct dotenv editor

--- Unity/Assets/UnityDotenv/Editors/DotenvConfigWindow.cs
+++ Unity/Assets/UnityDotenv/Editors/DotenvConfigWindow.cs
@@ -9,8 +9,15 @@ namespace UnityDotenv
 {
     public class DotenvConfigWindow : EditorWindow
     {
+        private Vector2 scrollPos = Vector2.zero;
+        private bool foldoutEnvDataSettings = true;
+        private bool isPreloadDotenv = true;
+        private Dictionary<string, string> envValuPair = new Dictionary<string, string>();
+        private string dotenvFilePath = ".env";
+
         void OnEnable()
         {
+            envValuPair = Env.Load(EnvPreloader.EnvFilePath).ToDictionary();
         }
 
         [MenuItem("Tools/Dotenv Config")]
@@ -22,9 +29,62 @@ namespace UnityDotenv
         // Windowのクライアント領域のGUI処理を記述
         void OnGUI()
         {
+            scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(".env file path");
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(Application.streamingAssetsPath);
+            string newDotenvFilePath = EditorGUILayout.TextField(dotenvFilePath);
+            if(dotenvFilePath != newDotenvFilePath)
+            {
+                dotenvFilePath = newDotenvFilePath;
+            }
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("preload .env file.");
+            isPreloadDotenv = EditorGUILayout.Toggle(isPreloadDotenv);
+            EditorGUILayout.EndHorizontal();
+
+            WithInFoldoutBlock(".env data params", ref foldoutEnvDataSettings, () =>
+            {
+                EditorGUILayout.BeginHorizontal();
+                for(int i = 0;i < 5; ++i)
+                {
+                    EditorGUILayout.Space();
+                }
+                if (GUILayout.Button("+"))
+                {
+                    if (!envValuPair.ContainsKey(""))
+                    {
+                        envValuPair.Add("", "");
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.Space();
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Key Name");
+                EditorGUILayout.LabelField("Value");
+                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.Space();
+                foreach (var kv in envValuPair)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.TextField(kv.Key);
+                    EditorGUILayout.TextField(kv.Value);
+                    EditorGUILayout.EndHorizontal();
+                    EditorGUILayout.Space();
+                }
+                if (GUILayout.Button("Save"))
+                {
+                }
+            });
+
+            EditorGUILayout.EndScrollView();
         }
 
-        void WithInFoldoutBlock(string title, ref bool foldout, Action callback)
+        private void WithInFoldoutBlock(string title, ref bool foldout, Action callback)
         {
             EditorGUI.indentLevel = 0;
             EditorGUILayout.BeginVertical(GUI.skin.box);
